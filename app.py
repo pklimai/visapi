fbfc9ad4a351ac0145dc907372328aa6e48d29c8
Peter Klimai <pklimai@gmail.com>
2023-08-21 23:05:04 +0300

    Some rework

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ app = Flask(__name__)
 from db import get_geometry_json
 
 @app.route('/')
-def hello():
-    with open("geometry_run_7-2076.json") as f:
+def dummy():
+    """ For testing - always return fixed JSON from base URL """
+    with open("examples/geometry_run_7-2076.json") as f:
         return "\n".join(f.readlines())
 
 @app.route('/geometry/<period_number>/<run_number>')
 def geometry(period_number, run_number):
+    """ Actually get geometry from ROOT and return it to client """
     return get_geometry_json(period_number, run_number)
 
